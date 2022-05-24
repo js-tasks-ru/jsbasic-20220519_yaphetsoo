8ffbf4e2cf9b3f98c330ade16b431592d4d6e668
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-05-24 23:49:03 +0300

    выполнен 1 модуль

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+
+  if (n == 0) {
+    return 1;
+  }
+
+  let result = n;
+
+  while (n > 1) {
+    result *= (n - 1);
+    n--;
+  }
+
+  return result;
 }
+
