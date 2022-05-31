b5947c79e5b65998cae175570c32786d9f2a5dc6
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-05-31 16:50:01 +0300

    Выполнен 3 модуль

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
   // ваш код...
+  let splitedArr = str.split('-');
+
+  if (splitedArr.length == 1) {
+    return splitedArr[0];
+  }
+
+  for (let i = 1; i < splitedArr.length; i++) {
+    let stringWithUpperCase = splitedArr[i].replace(splitedArr[i][0], splitedArr[i][0].toUpperCase());
+    splitedArr[i] = stringWithUpperCase;
+  }
+
+  return splitedArr.join("");
+
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
 function getMinMax(str) {
-  // ваш код...
+  let splitedArr = str.split(" ");
+  let numberArr = [];
+
+  splitedArr.forEach(element => {
+    if (!isNaN(Number(element))) {
+      numberArr.push(Number(element));
+    }
+  });
+
+  numberArr.sort(compare);
+  return { min: numberArr[0], max: numberArr[numberArr.length - 1]};
+}
+
+function compare(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
