7a2b9739f3fe51fd8fb5b3e13a17d7ac3ddfc91c
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-06-06 14:52:03 +0300

    Исправлена передача индексов в функциях check

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,11 +1,12 @@
+let indexes = { statusIndex: 0, genderIndex: 0, ageIndex: 0 };
+
 function highlight(table) {
-  let indexes = { statusIndex: 0, genderIndex: 0, ageIndex: 0 };
   setIndexes(table.rows[0], indexes);
 
   for (let i = 1; i < table.rows.length; i++) {
-    checkForStatus(table.rows[i], indexes.statusIndex);
-    checkForGender(table.rows[i], indexes.genderIndex);
-    checkForAge(table.rows[i], indexes.ageIndex);
+    checkForStatus(table.rows[i]);
+    checkForGender(table.rows[i]);
+    checkForAge(table.rows[i]);
   }
 }
 
@@ -25,26 +26,26 @@ function setIndexes(row, indexes) {
   }
 }
 
-function checkForStatus(row, statusIndex) {
-  if (row.cells[statusIndex].hasAttribute("data-available")) {
-    row.cells[statusIndex].dataset.available === "true" ? row.classList.add("available") : row.classList.add("unavailable");
+function checkForStatus(row) {
+  if (row.cells[indexes.statusIndex].hasAttribute("data-available")) {
+    row.cells[indexes.statusIndex].dataset.available === "true" ? row.classList.add("available") : row.classList.add("unavailable");
   }
   else {
     row.hidden = true;
   }
 }
 
-function checkForGender(row, genderIndex) {
-  if (row.cells[genderIndex].innerHTML === "m") {
+function checkForGender(row) {
+  if (row.cells[indexes.genderIndex].innerHTML === "m") {
     row.classList.add('male');
   }
-  else if (row.cells[genderIndex].innerHTML === "f") {
+  else if (row.cells[indexes.genderIndex].innerHTML === "f") {
     row.classList.add('female');
   }
 }
 
-function checkForAge(row, ageIndex) {
-  if (row.cells[ageIndex].innerHTML < 18) {
+function checkForAge(row) {
+  if (row.cells[indexes.ageIndex].innerHTML < 18) {
     row.style = "text-decoration: line-through";
   }
 }
