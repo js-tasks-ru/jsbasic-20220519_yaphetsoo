545d69aa4317ed47b8c75f748870488c39c24de2
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-06-11 18:47:19 +0300

    выполнен 5 модуль

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let leftCarouselButton = document.querySelector('.carousel__arrow_left');
+  let rightCarouselButton = document.querySelector('.carousel__arrow_right');
+  let carouselOffsetWidth = carousel.offsetWidth;
+
+  leftCarouselButton.style.display = 'none';
+  let currentSlide = 1;
+
+
+  leftCarouselButton.addEventListener("click", () => {
+    carousel.style.transform = `translateX(-${(currentSlide - 2) * carouselOffsetWidth}px)`;
+    currentSlide--;
+    setSlideButtons();
+  });
+
+  rightCarouselButton.addEventListener("click", () => {
+    carousel.style.transform = `translateX(-${currentSlide * carouselOffsetWidth}px)`;
+    currentSlide++;
+    setSlideButtons();
+  });
+
+  function setSlideButtons() {
+    if (currentSlide === 1) {
+      leftCarouselButton.style.display = 'none';
+    } else if (currentSlide === 4) {
+      rightCarouselButton.style.display = 'none';
+    } else {
+      leftCarouselButton.style.display = '';
+      rightCarouselButton.style.display = '';
+    }
+  }
 }
